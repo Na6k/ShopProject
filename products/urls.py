85f5775f2edbd8aa52af61f4f4211b0723fcf8e2
Nikita Lobanov <Na6k@mail.ru>
2023-02-28 12:05:53 +0300

    Optimized manufacturer_page, add value in request

--- products/urls.py
+++ products/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('group/<str:value>', views.group_page, name='group_page'),
     #path('names/<int:names_id>',views.group_page,name="names"),
     path('products/', views.products_page, name='products_page'),
-    path('manufacturer/<str:name>', views.manufacturer_page, name='manufacturer_page'),
+    path('manufacturer/<str:value>/<str:name>', views.manufacturer_page, name='manufacturer_page'),
     path('product/', views.product_page, name='product_page')
 ]
--- products/views.py
+++ products/views.py
@@ -13,31 +13,20 @@ def group_page(request, value):
         .filter(value = value)
         .prefetch_related('groups')
         )
-
     return render(request, 'group.html', {'names': category, 'current': value, 'group': group})
 
 
-def manufacturer_page(request, name):    
+def manufacturer_page(request, value,name):    
     grou = (
         Group.objects
         .filter(name = name)
         .prefetch_related('manufacturer')
-        )
-    category = (
-        Group.objects
-        .filter(name = name)
-        .values('names')
-        )
-    for el in category:
-        id_v = el
-    category = Names.objects.filter(pk = id_v['names']).values('value')
-    return render(request, 'manufacturer.html', {'grou':grou, "current": name, 'category':category})
+        )    
+    return render(request, 'manufacturer.html', {'grou':grou, "current": name, 'value': value})
 
 
 def products_page(request):
     mikado = Products.objects.filter(category_id = 1, group_id = 16, manufacturer_id = 95)
-    
-
     return render(request, 'products.html', {'mikado': mikado})
 
 
